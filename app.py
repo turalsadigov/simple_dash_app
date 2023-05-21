60afd5fca7cfc439a7627f11c22d89c152282429
Tural Sadigov <turalsadikov@gmail.com>
2023-05-20 17:16:28 -0700

    dash app

--- app.py
+++ app.py
@@ -1,17 +1,37 @@
-from flask import Flask, render_template
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
-app = Flask(__name__)
+app = dash.Dash(__name__)
+
+app.layout = html.Div(
+    [
+        html.H1("Perfect Square Checker"),
+        dcc.Input(
+            id="number-input",
+            type="number",
+            placeholder="Enter a number",
+            debounce=True,
+        ),
+        html.Div(id="result"),
+    ]
+)
 
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
-
-
-@app.route("/about")
-def about():
-    return "This is a simple Flask app."
+@app.callback(
+    dash.dependencies.Output("result", "children"),
+    [dash.dependencies.Input("number-input", "value")],
+)
+def check_perfect_square(number):
+    if number is not None:
+        square_root = number**0.5
+        if square_root == int(square_root):
+            return f"{number} is a perfect square!"
+        else:
+            return f"{number} is not a perfect square."
+    else:
+        return ""
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run_server(debug=True)
